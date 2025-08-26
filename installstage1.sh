1dde26f1be92b6e181ab756ecc57dc3c40328452
decentcarp <89420041+decentcarp@users.noreply.github.com>
2025-08-26 23:09:46 +0100

    Create installstage1.sh

---
+++ installstage1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+source /tmp/install-config.env
+
+mount /dev/$rootpart /mnt
+
+debootstrap --arch amd64 stable /mnt https://deb.debian.org/debian 
+mount --make-rslave --rbind /proc /mnt/proc 
+mount --make-rslave --rbind /sys /mnt/sys 
+mount --make-rslave --rbind /dev /mnt/dev 
+mount --make-rslave --rbind /run /mnt/run
+
+if [ -d /sys/firmware/efi/efivars/ ]; then
+    mkdir /mnt/efi
+    echo firmware=uefi >> /tmp/aerios-config.env
+    mount /dev/$bootefipart /mnt/efi
+else
+    echo firmware=bios >> /tmp/aerios-config.env
+    mount /dev/$bootefipart /mnt/boot
+fi
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+mkdir /mnt/tmp
+cp /tmp/installstage2.sh /mnt/tmp/
+cp /tmp/install-config.env /mnt/tmp/
+mkdir /mnt/aerios/
+
+chroot /mnt /tmp/installstage2.sh
